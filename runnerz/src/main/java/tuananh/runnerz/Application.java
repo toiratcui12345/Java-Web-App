4c3a94b6c355f39d4ffc4cdce81eb3c7003b50ee
Tuan Anh <tuananh9a1bn@gmail.com>
2025-09-06 23:31:56 +0700

    replace H2 database with a Postgres SQL database using Docker, using a Docker Compose, and then get into looking at some point in Spring Data

--- runnerz/src/main/java/tuananh/runnerz/Application.java
+++ runnerz/src/main/java/tuananh/runnerz/Application.java
@@ -2,16 +2,8 @@ package tuananh.runnerz;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import tuananh.runnerz.run.Location;
-import tuananh.runnerz.run.Run;
-import tuananh.runnerz.run.RunRepository;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
